03b416ace7e7266368b7f3747f829c207e2a5243
steffsch91 <steffsch@student.ethz.ch>
2015-07-31 21:30:56 +0100

    Did some stuff

--- foundation/newsglobe_subscriber.py
+++ foundation/newsglobe_subscriber.py
@@ -1,1 +1,9 @@
 __author__ = 'steffenschmidt'
+
+import feedparser
+
+d = feedparser.parse('http://rss.cnn.com/rss/edition.rss');
+print(d['feed']);
+print d.entries;
+for entry in d.entries:
+    print entry.published_parsed
