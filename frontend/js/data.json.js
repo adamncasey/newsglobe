25587ef7d20aa1c7607e0ab2c4800be18671c717
Adam Casey <adamncasey@gmail.com>
2015-08-01 02:14:43 +0100

    General check in

---
+++ frontend/js/data.json.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Adam on 31/07/2015.
+ */
+
+let data = {
+    10: 50.0,
+    11: 20.0,
+    15: 95.0,
+    16: 30
+};
+
+let countryNames = {
+    10: "United Kingdom",
+    11: "France",
+    15: "Germany",
+    16: "Russia"
+};
+
+function getMapData()
+{
+    return data;
+}
+
+function getCountryNames()
+{
+    return countryNames;
+}
