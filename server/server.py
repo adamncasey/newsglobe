98656ce36bc32787fd34d69655765cd63d2ba276
Antoine Giraudmaillet <antoine-g@live.fr>
2015-08-01 12:39:01 +0100

    New version of the server

--- server/server.py
+++ server/server.py
@@ -5,10 +5,10 @@ import json
 from flask import Flask
 import mysql.connector
 
-DB_USR = os.getenv('DB_USR', 'newsglobe')
-DB_PASS = os.environ.get('DB_PASS', 'test')
-DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
-DB_DB = os.environ.get('DB_DB', 'newsglobe')
+DB_USR = os.getenv('DB_USR', 'newsworld')
+DB_PASS = os.environ.get('DB_PASS', 'Newsworld0')
+DB_HOST = os.environ.get('DB_HOST', 'kim.antoinegiraudmaillet.com')
+DB_DB = os.environ.get('DB_DB', 'newsworld')
 
 app = Flask(__name__)
 cnx = mysql.connector.connect(user=DB_USR,
@@ -22,20 +22,21 @@ def hello():
     return "Hello World!"
 
 
-@app.route("/countrynews")
-def country_news():
+@app.route("/countrynews/<hours>")
+def country_news(hours=24):
+    print hours
     cursor = cnx.cursor()
-    query = ("SELECT country_code as country, COUNT(id) as count "
-             "FROM country_news_table "
-             "WHERE publish_date > DATE_SUB(NOW(), INTERVAL 1 HOUR "
-             "GROUP BY country_code;")
-
+    query = "SELECT country_code as country, COUNT(id) as count "
+    query += "FROM country_news_table "
+    query += "WHERE publish_date > DATE_SUB(NOW(), INTERVAL {} HOUR) ".format(hours)
+    query += "GROUP BY country_code;"
     cursor.execute(query)
-    response = dict
+    response = {}
+
     for (country, count) in cursor:
         response[country] = count
     return json.dumps(response)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
